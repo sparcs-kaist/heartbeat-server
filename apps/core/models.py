4c0f8bad49080f90039271d40f744364b25b0c04
Sam Jo <a1sams1a@gmail.com>
2016-11-08 21:12:49 +0900

    add (almost empty) api/update

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-# Create your models here.
 
-
-# To-do 
-# Server class should have the attribute about backup status
+# TODO: Server class should have the attribute about backup status
 class Server(models.Model):
     name = models.CharField(unique=True, max_length=30)
     alias = models.CharField(max_length=255)
     ip = models.GenericIPAddressField()
     key = models.CharField(max_length=255)
 
+
 class UsageLog(models.Model):
     server = models.ForeignKey('Server', db_index=True)
     datetime = models.DateTimeField() # 서버에서 저장시간 or client들이 정보 얻을때의 시간?
-    
+
+
 class CpuUsage(models.Model):
     usagelog = models.ForeignKey('UsageLog', db_index=True)
     user = models.DecimalField(max_digits=20, decimal_places=3) # 100%는 어떻게함
     system = models.DecimalField(max_digits=20, decimal_places=3)
     idle = models.DecimalField(max_digits=20, decimal_places=3)
+
 
 class MemoryUsage(models.Model):
     usagelog = models.ForeignKey('UsageLog', db_index=True)
@@ -28,7 +28,8 @@ class MemoryUsage(models.Model):
     virt_avail = models.BigIntegerField() # byte
     virt_used = models.BigIntegerField() # byte
     virt_total = models.BigIntegerField()
-    
+
+
 class DiskUsage(models.Model):
     usagelog = models.ForeignKey('UsageLog', db_index=True)
     device_name = models.CharField(max_length=30) # ex) dev/sda ...
@@ -37,7 +38,8 @@ class DiskUsage(models.Model):
     used = models.IntegerField() # byte
     total = models.IntegerField() # byte
 
-class ProcessUsage(models.Model): 
+
+class ProcessUsage(models.Model):
     PROCESSUSAGE_TYPE = (
         ('C','CPU'),
         ('M','Memory'),
@@ -46,20 +48,22 @@ class ProcessUsage(models.Model):
     usagelog = models.ForeignKey('UsageLog', db_index=True)
     name = models.CharField(max_length=127)
     order = models.IntegerField() #top 10 order
-
     cpu = models.DecimalField(max_digits=20, decimal_places=3)
     memory = models.DecimalField(max_digits=20, decimal_places=3)
     type = models.CharField(max_length=1, choices=PROCESSUSAGE_TYPE)
+
 
 class ErrorLog(models.Model):
     usagelog = models.ForeignKey('UsageLog', db_index=True)
     datetime = models.DateTimeField() # 서버저장시간 or client 정보 획득 시간?
     log = models.TextField()
 
+
 class BackupTarget(models.Model):
     server = models.ForeignKey('Server',db_index=True)
     path = models.CharField(max_length=255)
     period = models.CharField(max_length=30)
+
 
 class BackupLog(models.Model):
     server = models.ForeignKey('Server',db_index=True)
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,35 @@
+from django.http import JsonResponse
+from django.http.response import HttpResponseBadRequest, \
+    HttpResponseForbidden, HttpResponseNotFound, HttpResponseNotAllowed
 from django.shortcuts import render
+from core.models import Server
 
-# Create your views here.
+
+# /api/update
+def update(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed()
+
+    try:
+        data = json.loads(request.body)
+    except:
+        return HttpResponseBadRequest()
+
+    if 'server' not in data or \
+            'name' not in data['server'] or 'key' not in data['server']:
+        return HttpResponseBadRequest()
+
+    server_name = data['server']['name']
+    server_key = data['server']['key']
+
+    servers = Server.objects.filter(name=server_name)
+    if len(servers) == 0:
+        return HttpResponseNotFound()
+
+    server = servers[0]
+    if server.key != server_key:
+        return HttpReponseForbidden()
+
+    # update here
+
+    return JsonResponse({'success': True})
